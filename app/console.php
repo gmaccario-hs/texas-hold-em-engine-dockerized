f1dc08369a985ed9e61404a6dc2ce313e2be5ade
G.Maccario <g_maccario@hotmail.com>
2022-03-08 14:50:51 +0100

    Minor changes

--- app/console.php
+++ app/console.php
@@ -11,16 +11,20 @@ Author URI: giuseppemaccario.com
 
 require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-use TexasHoldem\Service\FileParser;
-use TexasHoldem\Engine\HandsEngine;
-use TexasHoldem\Command\HandsRulesEngineCommand;
 use Symfony\Component\Console\Application;
+use TexasHoldem\Command\HandsEngineCommand;
+use TexasHoldem\Engine\HandsEngine;
+use TexasHoldem\Service\FileParser;
 
 $appName = php_sapi_name() == 'cli' ? 'console' : 'http';
 
-if ($appName == 'console')
-{
-  $app = new Application();
-  $app->add(new HandsRulesEngineCommand(new FileParser(), new HandsEngine()));
-  $app->run();
+if ($appName == 'console') {
+    $app = new Application();
+    $app->add(new HandsEngineCommand(new FileParser(), new HandsEngine()));
+
+    try {
+        $app->run();
+    } catch (Exception $e) {
+        dump($e->getMessage());
+    }
 }
